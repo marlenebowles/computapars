c5a470c089b214be652f6b1cc86723c314ca3b17
Computapars <computapars@gmail.com>
2019-12-01 18:05:29 -0500

    Trying to reimplement icons

---
+++ packages/icon/index.story.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import { CheckCircleIcon } from './index';
+
+export default {
+	title: 'Components | Icon',
+};
+
+export const icons = () => (
+	<Fragment>
+		<CheckCircleIcon hover size={'24'} />
+	</Fragment>
+);
